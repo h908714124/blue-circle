f251adb2ca72b69c95add58a4e76ad7e0b5c4d4f
h908714124 <kraftdurchblumen@gmx.de>
2020-07-13 13:38:23 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 import {Point} from './modules/Point.js';
 
-const canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const ctx = canvas.getContext("2d");
-const r = Math.min(canvas.width / 2.0, canvas.height / 2.0) - 40;
-const x0 = canvas.width / 2;
-const y0 = canvas.height / 2;
+const w = window.innerWidth - 4;
+const h = window.innerHeight - 4;
+const r = Math.min(w / 2.0, h / 2.0) - 40;
+const x0 = w / 2;
+const y0 = h / 2;
 const points = [];
 for (let i = 0; i < 43; i++) {
     const phi = 2 * i * Math.PI * (1.0 / 43);
@@ -14,6 +12,12 @@ for (let i = 0; i < 43; i++) {
     const y = y0 + r * Math.sin(phi);
     points.push(new Point(x, y));
 }
+
+
+const canvas = document.getElementById('canvas');
+canvas.width = w;
+canvas.height = h;
+const ctx = canvas.getContext("2d");
 
 for (let point of points) {
     ctx.fillRect(point.x, point.y, 2, 2); // draw 4 pixel
